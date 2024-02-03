bc7d2a31d04cf83e0602144d0ec6eb1d51513d10
saarakhalid <khalidsaara6@gmail.com>
2024-02-03 18:01:59 +0300

    completed assignment

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function About({ image = "https://via.placeholder.com/215", aboutText }) {
+  return (
+    <aside>
+      <img src={image} alt="blog logo" />
+      <p>{aboutText}</p>
+    </aside>
+  );
+}
